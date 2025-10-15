7169cb31ed75e05929bc14dcc8a10b3d60e63ce3
Jenna Ofiara <jennaofiara@Jennas-MacBook-Pro-8.local>
2025-10-15 11:46:17 -0400

    almost done code with test cases

--- project_code.py
+++ project_code.py
@@ -1,15 +1,20 @@
 #Name: Jenna Ofiara
 #Email: Jofiara@umich.edu
 #Student ID: 73223467
-#Collaborators: GENAI (chatGPT)
+#Collaborators: GENAI (chatGPT) and my Tutor helped (worked alone though)
 
 import csv
 
 def load_penguins(filename):
     inFile = open(filename, newline='')
-    csv_reader = csv.DictReader(inFile) #GenAI told me to use this instead of .reader"
+    csv_reader = csv.DictReader(inFile)
     data = list(csv_reader)
 
+    print(" - Here are the headers: ", data[0].keys())
+    print(" - Here is a sample row: ", data[0])
+    print(" - Here are the number of rows:", len(data))
+    print("\n")
+    
     inFile.close()
     return data
 
@@ -18,25 +23,25 @@ def load_penguins(filename):
     for each species, depending on their sex'''
 
 def average_body_mass_by_species_sex(data):
-    groups = {}  # {species: {sex: [masses]}}
+    groups = {} #nested dict
     for row in data:
         if row["body_mass_g"] == "NA" or row["sex"] == "": #skip the rows without any data in it
             continue
-        species_mass = row["species"]
+        species_type = row["species"]
         species_sex = row["sex"]
-        mass = float(row["body_mass_g"])
-        if species_mass not in groups:
-            groups[species_mass] = {}
-        if species_sex not in groups[species_mass]:
-            groups[species_mass][species_sex] = []
-        groups[species_mass][species_sex].append(mass)
+        mass = float(row["body_mass_g"]) #change to float so it is not a string
+        if species_type not in groups:
+            groups[species_type] = {} #check is species exist
+        if species_sex not in groups[species_type]:
+            groups[species_type][species_sex] = []
+        groups[species_type][species_sex].append(mass) #check if the sex exists
 
-    results = []
-    for species_mass in groups:
-        for species_sex in groups[species_mass]:
-            values = groups[species_mass][species_sex]
-            average = round(sum(values)/len(values))
-            results.append((species_mass, species_sex, average))
+    results = [] #print results 
+    for species_type in groups:
+        for species_sex in groups[species_type]:
+            values = groups[species_type][species_sex]
+            average = round(sum(values)/len(values)) #find average 
+            results.append((species_type, species_sex, average))
     return results
 
 #average flipper length
@@ -48,36 +53,36 @@ def average_flipper_by_species_island(data):
     for row in data:
         if row["flipper_length_mm"] == "NA" or row["island"] == "":
             continue
-        species_mass = row["species"]
+        species_type = row["species"]
         island = row["island"]
         fl = float(row["flipper_length_mm"])
-        if species_mass not in groups:
-            groups[species_mass] = {}
-        if island not in groups[species_mass]:
-            groups[species_mass][island] = []
-        groups[species_mass][island].append(fl)
+        if species_type not in groups:
+            groups[species_type] = {}
+        if island not in groups[species_type]:
+            groups[species_type][island] = []
+        groups[species_type][island].append(fl)
 
     results = []
-    for species_mass in groups:
-        for island in groups[species_mass]:
-            value = groups[species_mass][island]
-            average = round(sum(value)/len(value))
-            results.append((species_mass, island, average))
+    for species_type in groups:
+        for island in groups[species_type]:
+            values = groups[species_type][island]
+            average = round(sum(values)/len(values))
+            results.append((species_type, island, average))
     return results
 
-#WRITE RESULTS
+#Write results (I had generative AI help me with this part because I struggled)
 '''write the results of the function'''
 def write_results(filename, mass_by_species_sex, flipper_by_species_island):
     with open(filename, "w") as f:
         # Body mass
         f.write("Average Body Mass (g) by Species and Sex\n")
-        for species_mass, species_sex, average in mass_by_species_sex:
-            f.write(f"{species_mass} ({species_sex}): {average}\n")
+        for species_type, species_sex, average in mass_by_species_sex:
+            f.write(f"{species_type} ({species_sex}): {average}\n")
 
         # Flipper length
         f.write("\nAverage Flipper Length (mm) by Species and Island\n")
-        for species_mass, island, average in flipper_by_species_island:
-            f.write(f"{species_mass} on {island}: {average}\n")
+        for species_type, island, average in flipper_by_species_island:
+            f.write(f"{species_type} on {island}: {average}\n")
 
     
 # --- main program ---
--- test_project.py
+++ test_project.py
@@ -1,7 +1,57 @@
 # test_project.py
 import unittest
-from project_code import average_body_mass_by_species_sex, average_flipper_by_species_island, write_results
+from project_code import average_body_mass_by_species_sex, average_flipper_by_species_island
 
+class TestAllMethods(unittest.TestCase):
+    def setUp(self):
+        #sample text examples to run 
+        self.SAMPLE = [
+            # Adelie on Torgersen (male and female)
+            {"species":"Adelie","island":"Torgersen","bill_length_mm":"39.1","bill_depth_mm":"18.7",
+             "flipper_length_mm":"190","body_mass_g":"3700","sex":"male","year":"2007"},
+            {"species":"Adelie","island":"Torgersen","bill_length_mm":"40.2","bill_depth_mm":"18.1",
+             "flipper_length_mm":"192","body_mass_g":"3800","sex":"female","year":"2007"},
+            # Gentoo on Biscoe (male and add skip NA because GENAI told me this was an important test case to run)
+            {"species":"Gentoo","island":"Biscoe","bill_length_mm":"46.1","bill_depth_mm":"14.8",
+             "flipper_length_mm":"215","body_mass_g":"5000","sex":"male","year":"2008"},
+            {"species":"Gentoo","island":"Biscoe","bill_length_mm":"47.5","bill_depth_mm":"15.0",
+             "flipper_length_mm":"217","body_mass_g":"NA","sex":"female","year":"2008"},
+        ]
+
+    # test average_body_mass_by_species_sex
+    def test_avg_mass_adelie(self):
+        res = {(species_type, species_sex): average for (species_type, species_sex, average) in average_body_mass_by_species_sex(self.SAMPLE)}
+        self.assertEqual(res[("Adelie","male")], 3700) #test male 
+        self.assertEqual(res[("Adelie","female")], 3800) #test female 
+
+    def test_avg_mass_gentoo(self): #however a skipped row 
+        res = {(species_type, species_sex): average for (species_type, species_sex, average) in average_body_mass_by_species_sex(self.SAMPLE)}
+        self.assertEqual(res[("Gentoo","male")], 5000) #test male 
+        self.assertNotIn(("Gentoo","female"), res)  # test female (NA)
+
+    def test_avg_mass_single_row(self):
+        data = [self.SAMPLE[0]]  # make sure handles small iput correctly 
+        self.assertEqual(average_body_mass_by_species_sex(data), [("Adelie","male",3700)])
+
+    def test_avg_mass_empty(self): #make sure handle empty list correctly 
+        self.assertEqual(average_body_mass_by_species_sex([]), [])
+
+    # ---------- average_flipper_by_species_island ----------
+    def test_avg_flipper_adelie_torgersen(self):
+        res = { (species_type, island): average for (species_type, island, average) in average_flipper_by_species_island(self.SAMPLE) }
+        self.assertEqual(res[("Adelie","Torgersen")], 191)  #taking male and female and dividing
+
+    def test_avg_flipper_gentoo_biscoe(self):
+        res = { (species_type, island): average for (species_type, island, average) in average_flipper_by_species_island(self.SAMPLE) }
+        self.assertEqual(res[("Gentoo","Biscoe")], 216)  #taking male and female and dividing
+
+    def test_avg_flipper_NA_rows(self):
+        data = [{"species":"Gentoo","island":"Biscoe","bill_length_mm":"46.1","bill_depth_mm":"14.8",
+             "flipper_length_mm":"NA","body_mass_g":"5000","sex":"male","year":"2008"}]
+        self.assertEqual(average_flipper_by_species_island(data), [])
+
+    def test_avg_flipper_empty(self):
+        self.assertEqual(average_flipper_by_species_island([]), [])
 
 
 if __name__ == "__main__":
